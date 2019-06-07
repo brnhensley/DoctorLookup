a37df02565c2b271effd65a5b480af2b952b7cdb
Brian Hensley <brnhensley@gmail.com>
2019-06-07 11:36:39 -0700

    delete logs

--- src/main.js
+++ src/main.js
@@ -13,14 +13,10 @@ $(document).ready(function () {
     $('#drname').val("");
     $('#medissue').val("");
     // console.log(process.env.API_KEY)
-    console.log("dr Name: ", drName, " ", "med Issue: " + medIssue);
     let doctorLookup = new DoctorLookup;
     let promise = doctorLookup.findDr(drName, medIssue);
     promise.then(function (response) {
       let body = JSON.parse(response);
-      console.log(body);
-      console.log(body.data.length);
-      console.log(body.data[0].profile.first_name, " ", body.data[0].practices[0].visit_address.street);
       if (body.data.length === 0) {
         $('#showDrs').text(`There are no doctors in Portland that match the search criteria.`)
       } else {
@@ -29,7 +25,7 @@ $(document).ready(function () {
           ${dr.practices[0].visit_address.street}<br>
           ${dr.practices[0].visit_address.city}, ${dr.practices[0].visit_address.state} ${dr.practices[0].visit_address.zip}<br>
           ${dr.practices[0].phones[0].number}<br>
-          <a href='${dr.profile.website}'>${dr.practices[0].website}</a><br>
+          <a href='${dr.profile.website}'>Website: ${dr.profile.website}</a><br>
           Accepting new patients: ${dr.practices[0].accepts_new_patients}<br>
           ${dr.profile.bio}<hr>`)
         }, function (error) {
