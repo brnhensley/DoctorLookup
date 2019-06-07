16f56dba3777b2e6d2b28278bd5c6b95a83ed200
Brian Hensley <brnhensley@gmail.com>
2019-06-07 09:57:22 -0700

    update incoming data to match response format

--- src/main.js
+++ src/main.js
@@ -18,11 +18,17 @@ $(document).ready(function () {
     promise.then(function (response) {
       let body = JSON.parse(response);
       console.log(body);
-      if (body.length === 0) { // IS THIS DATA AN ARRAY?
+      if (body.data.length === 0) { // IS THIS DATA AN ARRAY?
         $('#showDrs').text(`There are no doctors in Portland that match the search criteria.`)
       } else {
-        body.forEach(function (dr) {
-          $('#showDrs').append(`${dr.first_name} ${dr.last_name}<br>${dr.address}<br>${dr.city}, ${dr.state}, ${dr.postal_code}<br> ${dr.phone}<br> <a href='${dr.website_url}'>${dr.website_url}</a><hr>`)
+        body.data.forEach(function (dr) {
+          $('#showDrs').append(`${dr.profile.title} ${dr.profile.first_name} ${dr.profile.last_name}<br>
+          ${dr.practices.visit_address.street}<br>
+          ${dr.practices.visit_address.city}, ${dr.practices.visit_address.state} ${dr.practices.visit_address.zip}<br>
+          ${dr.practices.phones.number}<br>
+          <a href='${dr.profile.website}'>${dr.practices.website}</a><br>
+          Accepting new patients: ${dr.practices.accepts_new_patients}<br>
+          ${dr.profile.bio}<hr>`)
         }, function (error) {
           $('#showError').text(`There was an error processing your request: ${error.message}`);
         })
@@ -30,3 +36,7 @@ $(document).ready(function () {
     })
   })
 });
+
+`https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&location=or-portland&sort=first-name-asc&skip=0&limit=10&user_key=${process.env.apiKey}`
+
+query returns dob.doctor
