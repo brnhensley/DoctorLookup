2d4330e51791123b96c1e151f6d5e874092671ff
Brian Hensley <brnhensley@gmail.com>
2019-06-07 10:21:26 -0700

    add function to make api call

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,11 +1,19 @@
 export class DoctorLookup
 {
-  constructor(){
-    // this.something = something;
-  }
-
-  someFunction(){
-    // function code here
+  findDr(drName, medIssue){
+    return new Promise(funtion(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${drName}&query=${medIssue}&location=or-portland&sort=first-name-asc&skip=0&limit=10&user_key=${process.env.apiKey}`
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 
 }
