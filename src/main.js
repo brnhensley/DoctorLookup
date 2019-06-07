3d9e1932e52d89fe611d69af291e720714f55992
Brian Hensley <brnhensley@gmail.com>
2019-06-07 09:09:08 -0700

    add mock up of user logic

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
-import { ClassName } from './class-name.js';
+import { DoctorLookup } from './doctorLookup.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function() {
-  let className = new ClassName;
-  return className;
+$(document).ready(function () {
+  $('#drSearch').submit(function (event) {
+    event.preventDefault();
+    $('#showDrs').text("");
+    let drName = $('#drname').val();
+    let medIssue = $('#medissue').val();
+    $('#drname').val("");
+    $('#medissue').val("");
+    console.log("drName" + drName + " " + "medIssue" + medIssue);
+    let doctorLookup = new DoctorLookup;
+    let promise = doctorLookup.findDr(drName, medIssue);
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      if (body.length === 0) { // IS THIS DATA AN ARRAY?
+        $('#showDrs').text(`There are no doctors in Portland that match the search criteria.`)
+      } else {
+        body.forEach(function (dr) {
+          $('#showDrs').append(`${dr.first_name} ${dr.last_name}<br>${dr.address}<br>${dr.city}, ${dr.state}, ${dr.postal_code}<br> ${dr.phone}<br> <a href='${dr.website_url}'>${dr.website_url}</a><hr>`)
+        }, function (error) {
+          $('#showError').text(`There was an error processing your request: ${error.message}`);
+        })
+      }
+    })
+  })
 });
