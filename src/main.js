5511c6b87a234952783c787d5a77edb051da7fdf
Brian Hensley <brnhensley@gmail.com>
2019-06-07 16:55:13 -0700

    fix typos, add ;s and classes to dynamic html for styling

--- src/main.js
+++ src/main.js
@@ -17,32 +17,34 @@ $(document).ready(function () {
     promise.then(function (response) {
       let body = JSON.parse(response);
       if (body.data.length === 0) {
-        $('#showDrs').text(`There are no doctors in Portland that match the search criteria.`)
+        $('#showDrs').append(`<h4 class="pretty">There are no doctors in Portland that match the search criteria.</h4>`);
       } else {
         body.data.forEach(function (dr) {
           let newPatients = "";
 
           if (dr.profile.website === undefined){
-            dr.profile.website = "No Website Data"
+            dr.profile.website = "No Website Data";
           }
 
           if (dr.practices[0].accepts_new_patients === true) {
-            newPatients = `${dr.profile.title} ${dr.profile.last_name} is currently accepting new patients.`
+            newPatients = `${dr.profile.title} ${dr.profile.last_name} is currently accepting new patients.`;
           } else {
-            newPatients = `Unfortunatly ${dr.profile.title} ${dr.profile.last_name} is currently unable to accept new patients.`
+            newPatients = `Unfortunately ${dr.profile.title} ${dr.profile.last_name} is currently unable to accept new patients.`;
           }
 
-          $('#showDrs').append(`<strong>${dr.profile.title} ${dr.profile.first_name} ${dr.profile.last_name}</strong><br>
+          $('#showDrs').append(`<div class="pretty">
+          <strong>${dr.profile.title} ${dr.profile.first_name} ${dr.profile.last_name}</strong><br>
           <p>${dr.practices[0].visit_address.street}<br>
           ${dr.practices[0].visit_address.city}, ${dr.practices[0].visit_address.state} ${dr.practices[0].visit_address.zip}</p>
           <p>${dr.practices[0].phones[0].number}<br>
           Website: ${dr.profile.website}</p>
           <strong>${newPatients}</strong><br>
-          <p><em>${dr.profile.bio}</em></p><hr>`)
+          <p><em>${dr.profile.bio}</em></p></div><hr>`);
         }, function (error) {
-          $('#error').text(`There was an error processing your request: ${error.message}`)
-        })
+          $('#error').text(`There was an error processing your request: ${error.message}`);
+          $('#error').show();
+        });
       }
-    })
-  })
+    });
+  });
 });
