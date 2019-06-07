e664ebd21ae4999bf95104e8cb7b893a6272ed3f
Brian Hensley <brnhensley@gmail.com>
2019-06-07 13:33:41 -0700

    deleted unused files

--- spec/doctorLookup-spec.js
+++
@@ -1,9 +0,0 @@
-import { DoctorLookup } from './../src/doctorLookup.js';
-
-describe('DoctorLookup', function() {
-
-    it('should test something', function (){
-        //test goes here
-    });
-
-});
